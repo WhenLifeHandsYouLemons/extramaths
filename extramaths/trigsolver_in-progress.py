f645641b935fc9cb886a43f7d68009fd9006688c
Sooraj Sannabhadti <2005sooraj@gmail.com>
2021-10-07 17:15:45 +0800

    Update trigsolver_in-progress.py

--- extramaths/trigsolver_in-progress.py
+++ extramaths/trigsolver_in-progress.py
@@ -3,9 +3,13 @@ import math
 
 # Trigonometry solver
 def right_angle_trig(hypotenuse, opposite, adjacent, angle):
-    print(hypotenuse)
-    print(opposite)
-    print(adjacent)
-    print(angle)
+	if hypotenuse == 0:
+	    print(hypotenuse)
+	if opposite == 0:
+	    print(opposite)
+	if adjacent == 0:
+	    print(adjacent)
+	if angle == 0:
+	    print(angle)
 
 right_angle_trig(43, 23, 45, 4)
